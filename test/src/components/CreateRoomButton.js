519dab4ab42fce6f8e3135163163f64df45d209f
VD <vegetable0518meat@gmail.com>
2020-04-23 10:51:46 +0900

    環境整備

--- test/src/components/CreateRoomButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function createRoomButton({ roomPlus }) {
-    return (
-        <div>
-            <button onClick={() => roomPlus()}>ルーム作成</button>
-        </div>
-    )
-}
