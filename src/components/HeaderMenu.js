692f975216d189368cd1dfe2e6e9bdfc629a03c6
山田大二朗 <d-yamada@Regex.local>
2020-06-03 10:48:55 +0900

    Updata: ヘッダーメニュー外をクリックした時に、ヘッダーメニューを閉じる処理を追加

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import { ClickAwayListener } from '@material-ui/core';
 
 export default function HeaderMenu({ state, actions }) {
 	return (
-		// <div className="header-menu-wrapper" onMouseEnter={() => actions.showHeaderMenuTrue()} onMouseLeave={() => actions.showHeaderMenuFalse()}>
-		<div className="header-menu-wrapper" onClickAway={() => actions.showHeaderMenuFalse()} onClick={() => actions.showHeaderMenuTrue()}>
-			<div className="header-menu-icon">
-					<i class="fas fa-user-circle"></i>
-			</div>
-			{state.showFlag ?
-				<div className="header-menu">
-					<ul className="header-menu-list">
-						<li>Login</li>
-						<li>Sign Up</li>
-						<li>Profile</li>
-						<li>Config</li>
-						<li>Play History</li>
-					</ul>
-				</div>
-				:
-				null
-			}
-		</div>
+    <ClickAwayListener onClickAway={() => actions.showHeaderMenuFalse()}>
+      <div className="header-menu-wrapper" onClick={() => actions.showHeaderMenuTrue()} onMouseEnter={() => actions.showHeaderMenuTrue()} onMouseLeave={() => actions.showHeaderMenuFalse()}>
+        <div className="header-menu-icon">
+          <i class="fas fa-user-circle"></i>
+        </div>
+        {state.showFlag ?
+          <div className="header-menu">
+            <ul className="header-menu-list">
+              <li>Login</li>
+              <li>Sign Up</li>
+              <li>Profile</li>
+              <li>Config</li>
+              <li>Play History</li>
+            </ul>
+          </div>
+          :
+          null
+        }
+      </div>
+    </ClickAwayListener>
 	)
 }
