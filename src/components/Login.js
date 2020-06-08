bcad232df03dc324e0ec5c7cf8179cceeae17ace
ra-i-to <rightning83@gmail.com>
2020-06-08 13:12:15 +0900

    ログイン画面デザイン作成

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ import Footer from './Footer';
 import Body from './Body';
 import CenterMainBody from './CenterMainBody';
 import Modal from '../containers/ModalContainer';
+import BodyHeader from './BodyHeader';
 
 import './css/Login.css';
 
@@ -15,7 +16,17 @@ export default function Login() {
       <Header />
       <Body>
         <CenterMainBody>
-          
+          <BodyHeader>
+            ログイン
+          </BodyHeader>
+          <div className="button-area">
+            <button className="facebook">Facebookアカウントでログイン</button>
+            <button className="twitter">Twitterアカウントでログイン</button>
+            <button className="google">Googleアカウントでログイン</button>
+          </div>
+          <div className="register-link-area">
+            <Link>新規登録</Link>はこちら
+          </div>
         </CenterMainBody>
       </Body>
       <Footer />
