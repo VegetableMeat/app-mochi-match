831750501ddaf894e7414cc3358abc9ce654e07e
123456789-cell <pondelion991122@icloud.com>
2020-06-11 09:37:18 +0900

    add:設定画面枠組み追加

--- src/components/DangerButton.js
+++ src/components/DangerButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export default function HeadLine1(props) {
+export default function DangerButton(props) {
     return (
         <div>
             {/* {props.children} */}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import store from './Store';
 import Top from './components/Top';
 import GameCreate from './components/GameCreate';
 import History from './components/History';
+import Configuration from './components/Configuration';
 import Login from './components/Login';
 
 ReactDOM.render(
@@ -15,6 +16,7 @@ ReactDOM.render(
       <Route exact path='/' component={Top} />
       <Route exact path='/gameCreate' component={GameCreate} />
       <Route exact path='/history' component={History} />
+      <Route exact path='/configuration' component={Configuration} />
       <Route exact path='/login' component={Login} />
     </ BrowserRouter>
   </Provider>,
