08c422402cc24e0fc429f92d4907335cf9efe5b4
raito-aug-3 <yamanaka@esupport.co.jp>
2020-06-15 09:37:36 +0900

    プレイ履歴画面の作成

--- src/components/History.js
+++ src/components/History.js
@@ -12,6 +12,9 @@ import UnderLineList from './UnderLineList';
 import RadiusBodyHeader from './RadiusBodyHeader';
 import RadiusWhiteCard from './RadiusWhiteCard';
 import UserPlate from './UserPlate';
+import HeadeLine1 from './HeadLine1';
+import InnerText from './InnerText';
+import BreakUnderLine from './BreakUnderLine';
 
 import './css/History.css';
 
@@ -21,19 +24,50 @@ export default function History() {
             <Header />
             <Body>
                 <SideMenu>
-                    <div className="menu-wrapper menu-wrapper-1">
+                    <div className="menu-wrapper menu-wrapper-1 account-side-menu">
                         <MenuHeader text="メニュー" />
                         <MenuInnerWrapper>
-                            
+                        <ul>
+                            <Link to="/"><li><i class="far fa-id-card"></i>Profile</li></Link>
+                            <Link to="/"><li><i class="fas fa-user-cog"></i>Setting</li></Link>
+                            <Link to="/History"><li><i class="fas fa-history"></i>Play History</li></Link>
+                        </ul>
                         </MenuInnerWrapper>
                     </div>
                     <UnderLineList />
                 </SideMenu>
                 <MainBody>
-                    <RadiusBodyHeader />
-                    <RadiusWhiteCard>
-                        <UserPlate />
-                    </RadiusWhiteCard>
+                    <RadiusBodyHeader title="プレイ履歴" />
+                    <div className="history-cards">
+                        <RadiusWhiteCard>
+                            <div className="line">
+                                <HeadeLine1>プレイ日時</HeadeLine1>
+                                <InnerText>4月6日　10時22分</InnerText>
+                            </div>
+                            <BreakUnderLine />
+                            <div className="line">
+                                <HeadeLine1>ホスト</HeadeLine1>
+                                <InnerText>もちもちくん</InnerText>
+                            </div>
+                            <BreakUnderLine />
+                            <div className="line">
+                                <HeadeLine1>ゲーム</HeadeLine1>
+                                <InnerText>あつまれどうぶつの盛り</InnerText>
+                            </div>
+                            <BreakUnderLine />
+                            <div className="multi-line">
+                                <HeadeLine1>メンバー</HeadeLine1>
+                                <div className="members-area">
+                                    <div className="user-plate">TestPlate</div>
+                                    <div className="user-plate">TestPlate</div>
+                                    <div className="user-plate">TestPlate</div>
+                                    <div className="user-plate">TestPlate</div>
+                                    <div className="user-plate">TestPlate</div>
+                                    <div className="user-plate">TestPlate</div>
+                                </div>
+                            </div>
+                        </RadiusWhiteCard>
+                    </div>
                 </MainBody>
             </Body>
             <Footer />
--- src/components/RadiusWhiteCard.js
+++ src/components/RadiusWhiteCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import './css/RadiusWhiteCard.css';
 
 export default function RadiusWhiteCard(props) {
     return (
-        <div>
-            {/* {props.children} */}
+        <div className="radius-white-card">
+            {props.children}
         </div>
     )
 }
