f6ecbe8e9a475eb9e9ca168e03348424dfbdbde1
taniwhy <>
2020-08-05 19:30:54 +0900

    人気ゲームの表示

--- src/components/PopularGames.jsx
+++ src/components/PopularGames.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const PopularGames = ({ state }) => {
+const PopularGames = ({ state, actions }) => {
+  useEffect(() => {
+    actions.getPopularGamesReq();
+  }, []);
+
+  console.log(state);
   return (
     <ol>
       {state.data.map((value, key) => (
-        <li key={key}>{value.popular}</li>
+        <li key={key}>{value.game_title}</li>
       ))}
     </ol>
   );
