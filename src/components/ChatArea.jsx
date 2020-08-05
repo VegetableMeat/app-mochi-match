c6508d4857ae63aad6d1a4503482d02ffd0ede91
taniwhy <>
2020-08-05 23:08:03 +0900

    チャットログのバグを修正

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -67,7 +67,9 @@ const ChatArea = ({ actions, state }) => {
   }, [roomState.chatLog]);
 
   useEffect(() => {
-    if (scrollTop > chatlogEl.current.scrollHeight - 620) {
+    console.log(scrollTop, chatlogEl.current.scrollHeight - 650);
+    if (scrollTop > chatlogEl.current.scrollHeight - 650) {
+      console.log("ok");
       setNewMessageCnt(0);
       setBeforeMessageCnt(chatLog.length);
       setNewMessageFlg(false);
